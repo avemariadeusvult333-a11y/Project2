f494c649e6c6d74e7a5e2471aca2aeede6e484da
avemariadeusvult333-a11y <avemariadeusvult333@gmail.com>
2026-03-06 20:09:22 +0300

    изменения2

--- scripts/Untitled-1.js
+++ scripts/Untitled-1.js
@@ -1,2 +1,44 @@
-let elements = document.getElementsByClassName("header") 
-document.getElementById("header").innerText = "a;oegjtruhi"
+const coolGames = [
+  {
+    name: "Metro Exodus",
+    href: "pages/Metro.html",
+    year: 2019,
+    rating: 8.5,
+    multiplayer: false
+  },
+  {
+    name: "Resident Evil 3",
+    href: "pages/ResdidentEvil.html",
+    year: 2020,
+    rating: 8.0,
+    multiplayer: false
+  },
+  {
+    name: "The Witcher 3",
+    href: "pages/TheWitcher3.html",
+    year: 2015,
+    rating: 9.5,
+    multiplayer: false
+  }
+];
+const container = document.querySelector("#games");
+coolGames.forEach(game => {
+  let status;
+  if (game.year > 2020) {
+    status = "Новая игра";
+  } else {
+    status = "Классика";
+  }
+  const card = document.createElement("div");
+
+  card.innerHTML = `
+    <h3>${game.name}</h3>
+    <p>Год: ${game.year}</p>
+    <p>Рейтинг: ${game.rating}</p>
+    <p>Мультиплеер: ${game.multiplayer ? "да" : "нет"}</p>
+    <p>${status}</p>
+    <a href="${game.href}">Открыть страницу</a>
+  `;
+  container.appendChild(card);
+});
+
